889d2273ee85b898754b48cc43c932e24cbb48ee
limjiin <limjiin0413@gmail.com>
2021-08-23 14:04:43 +0900

    feat: feature caesar cipher done

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,5 +1,15 @@
-#description
+message = 'GIEWIVrGMTLIVrHIQS' #encrypted message
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-- Caesar Cipher Technique is the simple and easy method of encryption technique.
-- It is simple type of substitution cipher.
-- Each letter of plain text is replaced by a letter with some fixed number of positions down with alphabet.
+for key in range(len(LETTERS)):
+   translated = ''
+   for symbol in message:
+      if symbol in LETTERS:
+         num = LETTERS.find(symbol)
+         num = num - key
+         if num < 0:
+            num = num + len(LETTERS)
+         translated = translated + LETTERS[num]
+      else:
+         translated = translated + symbol
+print('Hacking key #%s: %s' % (key, translated))
